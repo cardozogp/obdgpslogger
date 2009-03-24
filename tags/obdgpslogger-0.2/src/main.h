dbaf73732c0b04b704309b12a9f03765f7e19d57
chunky <chunky@ec62d176-4a65-0410-9229-e4683bb12484>
2009-03-24 17:47:34 +0000

    Tagging this as 0.2

---
+++ tags/obdgpslogger-0.2/src/main.h
@@ -0,0 +1,53 @@
+/* Copyright 2009 Gary Briggs
+
+This file is part of obdgpslogger.
+
+obdgpslogger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+obdgpslogger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with obdgpslogger.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/** \file
+ \brief main obdlogger entrypoint
+ */
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include <getopt.h>
+#include <stdlib.h>
+
+/// getopt() long options
+static const struct option longopts[] = {
+	{ "help", no_argument, NULL, 'h' }, ///< Print the help text
+	{ "version", no_argument, NULL, 'v' }, ///< Print the version text
+	{ "serial", required_argument, NULL, 's' }, ///< Serial Port
+	{ "samplerate", required_argument, NULL, 'a' }, ///< Number of samples per second
+	{ "count", required_argument, NULL, 'c' }, ///< Number of values to grab
+	{ "db", required_argument, NULL, 'd' }, ///< Database file
+	{ NULL, 0, NULL, 0 } ///< End
+};
+
+/// getopt() short options
+static const char shortopts[] = "hvs:d:c:a:";
+
+/// Print Help for --help
+/** \param argv0 your program's argv[0]
+ */
+void printhelp(const char *argv0);
+
+/// Print the version string
+void printversion();
+
+
+#endif // __MAIN_H
+
