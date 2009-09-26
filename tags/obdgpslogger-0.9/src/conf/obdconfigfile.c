15a81c8dd64560aedab5f81ae634c8ac7783d8f0
chunky <chunky@ec62d176-4a65-0410-9229-e4683bb12484>
2009-09-26 05:08:55 +0000

    Tagging 0.9 release

---
+++ tags/obdgpslogger-0.9/src/conf/obdconfigfile.c
@@ -0,0 +1,193 @@
+/* Copyright 2009 Gary Briggs
+
+This file is part of obdgpslogger.
+
+obdgpslogger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+obdgpslogger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with obdgpslogger.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** \file
+ \brief Tool-wide configuration
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "obdconfig.h"
+#include "obdservicecommands.h"
+#include "obdconfigfile.h"
+
+#ifndef MAX_PATH
+#define MAX_PATH 1024
+#endif //MAX_PATH
+
+/// Get "a" valid home dir in which to store a dotfile
+static const char *getPlatformHomeDir() {
+	static char homedir[MAX_PATH] = "\0";
+
+	char *envhome;
+	if(0 != strlen(homedir)) {
+		return homedir;
+	} else {
+		// On OSX, might want to suffix "Application Data" ?
+		envhome = getenv("HOME");
+		if(NULL != envhome && *envhome) {
+			snprintf(homedir,sizeof(homedir), "%s/", envhome);
+			return homedir;
+		}
+
+		// Smells like windows
+		envhome = getenv("APPDATA");
+		if(NULL != envhome && *envhome) {
+			snprintf(homedir,sizeof(homedir), "%s", envhome);
+			return homedir;
+		}
+
+		snprintf(homedir,sizeof(homedir), ".");
+	}
+	
+	return homedir;
+}
+
+static int obd_parseConfig(FILE *f, struct OBDGPSConfig *c, int verbose) {
+	char line[1024];
+	while(NULL != fgets(line,sizeof(line),f)) {
+		// If you change the 1024 here, be damn sure to change it in the sscanf too
+		char singleval_s[1024];
+		int singleval_i;
+		if(1 == sscanf(line, "obddevice=%1023s", singleval_s)) {
+			if(NULL != c->obd_device) {
+				free((void *)c->obd_device);
+			}
+			c->obd_device = strdup(singleval_s);
+			if(verbose) printf("Conf Found OBD Device: %s\n", singleval_s);
+		}
+		if(1 == sscanf(line, "gpsdevice=%1023s", singleval_s)) {
+			if(NULL != c->gps_device) {
+				free((void *)c->gps_device);
+			}
+			c->gps_device = strdup(singleval_s);
+			if(verbose) printf("Conf Found GPS Device: %s\n", singleval_s);
+		}
+		if(1 == sscanf(line, "log_columns=%1023s", singleval_s)) {
+			if(NULL != c->log_columns) {
+				free((void *)c->log_columns);
+			}
+			c->log_columns = strdup(singleval_s);
+			if(verbose) printf("Conf Found log_columns: %s\n", singleval_s);
+		}
+		if(1 == sscanf(line, "samplerate=%i", &singleval_i)) {
+			c->samplerate = singleval_i;
+			if(verbose) printf("Conf Found samplerate: %i\n", singleval_i);
+		}
+		if(1 == sscanf(line, "optimisations=%i", &singleval_i)) {
+			c->optimisations = singleval_i;
+			if(verbose) printf("Conf Found optimisations: %i\n", singleval_i);
+		}
+	}
+	return 0;
+}
+
+struct OBDGPSConfig *obd_loadConfig(int verbose) {
+	struct OBDGPSConfig *c = (struct OBDGPSConfig *)malloc(sizeof(struct OBDGPSConfig));
+	if(NULL == c) return NULL;
+	c->obd_device = strdup(OBD_DEFAULT_SERIALPORT);
+	c->gps_device = strdup(OBD_DEFAULT_GPSPORT);
+	c->log_columns = strdup(OBD_DEFAULT_COLUMNS);
+	c->samplerate = 1;
+	c->optimisations = 0;
+
+	char fullfilename[MAX_PATH];
+
+	// For portableness back to older windows, '/' separator won't work
+	snprintf(fullfilename, sizeof(fullfilename), "%s/%s",
+			getPlatformHomeDir(), OBD_CONFIG_FILENAME
+			);
+
+	FILE *f = fopen(fullfilename, "r");
+
+	if(NULL != f) {
+		obd_parseConfig(f,c,verbose);
+		fclose(f);
+	}
+
+	if(verbose) {
+		printf("Full Config:\n"
+					 "	obddevice:%s\n"
+					 "	gpsdevice:%s\n"
+					 "	logcolumns:%s\n"
+					 "	optimisations:%i\n"
+					 "	samplerate:%i\n",
+					 	c->obd_device, c->gps_device, c->log_columns,
+						c->optimisations, c->samplerate);
+	}
+	return c;
+}
+
+/// Free a config created by loadOBDGPSConfig
+void obd_freeConfig(struct OBDGPSConfig *c) {
+	if(NULL == c) return;
+	if(NULL != c->obd_device) free((void *)c->obd_device);
+	if(NULL != c->gps_device) free((void *)c->gps_device);
+	if(NULL != c->log_columns) free((void *)c->log_columns);
+	free(c);
+}
+
+int obd_configCmds(const char *log_columns, struct obdservicecmd ***cmds) {
+	int cols = 0;
+	*cmds = NULL;
+	const char *toklist=",: "; // Seriously, we only want comma-separated though
+
+	// Goind to do this twice. First time, get a count to allocate.
+	char *cmdlist = strdup(log_columns);
+	if(NULL == cmdlist) return -1;
+
+	char *currcmd = strtok(cmdlist, toklist);
+	while(currcmd) {
+		struct obdservicecmd *c;
+		if(NULL != (c = obdGetCmdForColumn(currcmd))) {
+			cols++;
+		} else {
+			printf("Warning: Couldn't find column '%s'. Possible config file problem\n", currcmd);
+		}
+		currcmd = strtok(NULL, toklist);
+	}
+	free((void *)cmdlist);
+
+	// Second time, do the allocation then the assignement
+	*cmds = (struct obdservicecmd **)malloc((cols+1) * sizeof(struct obdservicecmd *)); // 1 == NULL sentinel
+
+	int currcol = 0;
+	cmdlist = strdup(log_columns);
+	if(NULL == cmdlist) return -1;
+
+	currcmd = strtok(cmdlist, toklist);
+	while(currcmd) {
+		struct obdservicecmd *c;
+		if(NULL != (c = obdGetCmdForColumn(currcmd))) {
+			(*cmds)[currcol++] = c;
+		}
+		currcmd = strtok(NULL, toklist);
+	}
+	free((void *)cmdlist);
+
+	(*cmds)[currcol] = NULL;
+
+	return cols;
+}
+
+void obd_freeConfigCmds(struct obdservicecmd **cmds) {
+	free((void *)cmds);
+}
+
---
+++ tags/obdgpslogger-0.9/src/logger/obdserial.c
@@ -0,0 +1,237 @@
+/* Copyright 2009 Gary Briggs
+
+This file is part of obdgpslogger.
+
+obdgpslogger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+obdgpslogger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with obdgpslogger.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/** \file
+ \brief serial stuff
+ */
+
+// Code and ideas borrowed in places from
+// http://easysw.com/~mike/serial/serial.html
+
+#include "obdserial.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+
+/// What to use as the obd newline char in commands
+#define OBDCMD_NEWLINE "\r"
+
+/// Handle to the serial log
+static FILE *seriallog = NULL;
+
+/// Write to the log
+static void appendseriallog(const char *line) {
+	if(NULL != seriallog) {
+		fprintf(seriallog, "%s", line);
+		fflush(seriallog);
+	}
+}
+
+/// Throw away all data until the next prompt
+void readtonextprompt(int fd) {
+	char retbuf[4096]; // Buffer to store returned stuff
+	char *bufptr = retbuf; // current position in retbuf
+
+	int nbytes; // Number of bytes read
+	while(0 < (nbytes = read(fd,bufptr, retbuf+sizeof(retbuf)-bufptr-1))) {
+		bufptr += nbytes;
+		if(bufptr[-1] == '>') {
+			break;
+		}
+	}
+	appendseriallog(retbuf);
+}
+
+// Blindly send a command and throw away all data to next prompt
+/**
+ \param cmd command to send
+ \param fd file descriptor
+ */
+void blindcmd(int fd, const char *cmd) {
+	appendseriallog(cmd);
+	write(fd,cmd, strlen(cmd));
+	readtonextprompt(fd);
+}
+
+int openserial(const char *portfilename) {
+	struct termios options;
+	int fd;
+
+	fprintf(stderr,"Opening serial port %s, this can take a while\n", portfilename);
+	// MIGHT WANT TO REMOVE O_NDELAY
+	fd = open(portfilename, O_RDWR | O_NOCTTY | O_NDELAY);
+
+	if(fd == -1) {
+		perror(portfilename);
+	} else {
+		fcntl(fd, F_SETFL, 0);
+
+		// Get the current options for the port
+		tcgetattr(fd, &options);
+
+		options.c_cflag |= (CLOCAL | CREAD);
+		options.c_lflag &= !(ICANON | ECHO | ECHOE | ISIG);
+		options.c_oflag &= !(OPOST);
+		options.c_cc[VMIN] = 0;
+		options.c_cc[VTIME] = 100;
+
+		tcsetattr(fd, TCSANOW, &options);
+
+
+		// Now some churn to get everything up and running.
+		blindcmd(fd,"" OBDCMD_NEWLINE);
+		// Reset the device. Some software changes settings and then leaves it
+		blindcmd(fd,"ATZ" OBDCMD_NEWLINE);
+		// Do a general cmd that all obd-devices support
+		blindcmd(fd,"0100" OBDCMD_NEWLINE);
+		// Disable command echo [elm327]
+		blindcmd(fd,"ATE0" OBDCMD_NEWLINE);
+		// Don't insert spaces [readability is for ugly bags of mostly water]
+		blindcmd(fd,"ATS0" OBDCMD_NEWLINE);
+
+	}
+	return fd;
+}
+
+void closeserial(int fd) {
+	blindcmd(fd,"ATZ" OBDCMD_NEWLINE);
+	close(fd);
+}
+
+int startseriallog(const char *logname) {
+	if(NULL == (seriallog = fopen(logname, "w"))) {
+		perror("Couldn't open seriallog");
+		return 1;
+	}
+	return 0;
+}
+
+void closeseriallog() {
+	fflush(seriallog);
+	fclose(seriallog);
+	seriallog = NULL;
+}
+
+enum obd_serial_status getobdbytes(int fd, unsigned int cmd, int numbytes_expected,
+	unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D, int *numbytes_returned) {
+
+	char sendbuf[20]; // Command to send
+	int sendbuflen; // Number of bytes in the send buffer
+
+	char retbuf[4096]; // Buffer to store returned stuff
+	char *bufptr; // current position in retbuf
+
+	int nbytes; // Number of bytes read
+	// int tries; // Number of tries so far
+
+	if(0 == numbytes_expected) {
+		sendbuflen = snprintf(sendbuf,sizeof(sendbuf),"01%02X" OBDCMD_NEWLINE, cmd);
+	} else {
+		sendbuflen = snprintf(sendbuf,sizeof(sendbuf),"01%02X%01X" OBDCMD_NEWLINE, cmd, numbytes_expected);
+	}
+
+	appendseriallog(sendbuf);
+	if(write(fd,sendbuf,sendbuflen) < sendbuflen) {
+		return OBD_ERROR;
+	}
+
+	bufptr = retbuf;
+	while(0 < (nbytes = read(fd,bufptr, retbuf+sizeof(retbuf)-bufptr-1))) {
+		bufptr += nbytes;
+		if(bufptr[-1] == '>') {
+			break;
+		}
+	}
+	appendseriallog(retbuf);
+
+	*bufptr = '\0';
+
+	unsigned int response; // Response. Should always be 0x41
+	unsigned int mode; // Mode returned [should be the same as cmd]
+	int count; // number of retvals successfully sscanf'd
+
+	count = sscanf(retbuf, "%2x %2x %2x %2x %2x %2x", &response, &mode,
+					A,B,C,D);
+
+	if(NULL != strstr(retbuf, "NO DATA")) {
+		fprintf(stderr, "OBD reported NO DATA for cmd %02X: %s\n", cmd, retbuf);
+		return OBD_NO_DATA;
+	}
+
+	if(count <= 2) {
+		fprintf(stderr, "Didn't get parsable data back for cmd %02X: %s\n", cmd, retbuf);
+		return OBD_UNPARSABLE;
+	}
+	if(response != 0x41) {
+		fprintf(stderr, "Didn't get successful response for cmd %02X: %s\n", cmd, retbuf);
+		return OBD_INVALID_RESPONSE;
+	}
+	if(mode != cmd) {
+		fprintf(stderr, "Didn't get returned data we wanted for cmd %02X: %s\n", cmd, retbuf);
+		return OBD_INVALID_MODE;
+	}
+
+	*numbytes_returned = count-2;
+	return OBD_SUCCESS;
+}
+
+enum obd_serial_status getobdvalue(int fd, unsigned int cmd, float *ret, int numbytes, OBDConvFunc conv) {
+	int numbytes_returned;
+	unsigned int fourbytes[4];
+
+	enum obd_serial_status ret_status = getobdbytes(fd, cmd, numbytes,
+		&fourbytes[0], &fourbytes[1], &fourbytes[2], &fourbytes[3],
+		&numbytes_returned);
+
+	if(OBD_SUCCESS != ret_status) return ret_status;
+
+	if(NULL == conv) {
+		int i;
+		*ret = 0;
+		for(i=0;i<numbytes_returned;i++) {
+			*ret = *ret * 256;
+			*ret = *ret + fourbytes[i];
+		}
+	} else {
+		*ret = conv(fourbytes[0], fourbytes[1], fourbytes[2], fourbytes[3]);
+	}
+	return OBD_SUCCESS;
+}
+
+int getnumobderrors(int fd) {
+	int numbytes_returned;
+	unsigned int fourbytes[4];
+	
+	enum obd_serial_status ret_status = getobdbytes(fd, 0x01, 0,
+		&fourbytes[0], &fourbytes[1], &fourbytes[2], &fourbytes[3],
+		&numbytes_returned);
+	
+	if(OBD_SUCCESS != ret_status || 0 == numbytes_returned) return 0;
+	
+	if(fourbytes[0] > 0) {
+		return fourbytes[0] & 0x7F;
+	}
+	
+	return 0;
+}
+
