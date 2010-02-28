76ef2eea07fc33ecb2f45efafca4dc5456224fff
chunky <chunky@ec62d176-4a65-0410-9229-e4683bb12484>
2010-02-28 00:49:40 +0000

    Tagging 0.13 release

---
+++ tags/obdgpslogger-0.13/src/analysis/analysistables.h
@@ -0,0 +1,31 @@
+#ifndef __ANALYSISTABLES_H
+#define __ANALYSISTABLES_H
+
+#include "sqlite3.h"
+
+/// Create the analysis tables we need in this database
+int createAnalysisTables(sqlite3 *db);
+
+/// Purge any analyiss data from the tables we've created
+int resetTripAnalysisTables(sqlite3 *db);
+
+/// Insert [or update] these parameters for this trip
+int insertTripAnalysis(sqlite3 *db, int trip, double length,
+	double meanlat, double meanlon, double medianlat, double medianlon);
+
+/// Get the parameters for the requested trip
+int getTripAnalysis(sqlite3 *db, int trip, double *length,
+	double *meanlat, double *meanlon, double *medianlat, double *medianlon);
+
+/// Populate median-median mean-mean O(n^2) table
+int meanMedianDistances(sqlite3 *db);
+
+/// Populate the analysis tables
+int fillAnalysisTables(sqlite3 *db);
+
+/// Export gps analysis to CSV
+int exportGpsCSV(sqlite3 *db, FILE *f);
+
+#endif // __ANALYSISTABLES_H
+
+
---
+++ tags/obdgpslogger-0.13/src/logger/gpscomm.c
@@ -0,0 +1,71 @@
+/* Copyright 2009 Gary Briggs
+
+This file is part of obdgpslogger.
+
+obdgpslogger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+obdgpslogger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with obdgpslogger.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/** \file
+ \brief gps communications
+ */
+
+#ifdef HAVE_GPSD
+
+#include <gps.h>
+
+struct gps_data_t *opengps(char *server, char *port) {
+	struct gps_data_t *g = gps_open(server,port);
+	if(NULL == g)
+		return NULL;
+
+#ifdef HAVE_GPSD_V3
+	gps_stream(g, WATCH_ENABLE, NULL);
+#else
+	gps_query(g, "o");
+#endif //HAVE_GPSD_V3
+
+	return g;
+}
+
+void closegps(struct gps_data_t *g) {
+	gps_close(g);
+}
+
+int getgpsposition(struct gps_data_t *g, double *lat, double *lon, double *alt) {
+#ifdef HAVE_GPSD_V3
+	gps_poll(g);
+#else
+	gps_query(g, "o");
+#endif //HAVE_GPSD_V3
+	if(g->fix.mode < MODE_2D) {
+		return -1;
+	}
+	if(g->fix.mode == MODE_2D) {
+		*lat = g->fix.latitude;
+		*lon = g->fix.longitude;
+		return 0;
+	}
+	if(g->fix.mode == MODE_3D) {
+		*lat = g->fix.latitude;
+		*lon = g->fix.longitude;
+		*alt = g->fix.altitude;
+		return 1;
+	}
+	// Shouldn't be able to get to here...
+	return -1;
+}
+
+#endif //HAVE_GPSD
+
